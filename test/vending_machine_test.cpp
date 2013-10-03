cc548456b0b12b051290806c9bd32598abfd5098
Rob Westgeest <rob@qwan.it>
2013-10-03 08:39:17 +0200

    first simple tests as if we reaally meant it

--- test/vending_machine_test.cpp
+++ test/vending_machine_test.cpp
@@ -1,7 +1,42 @@
 #include "gtest/gtest.h"
+#include <set>
 
+enum CanType { None, Cola, Fanta };
+
+CanType delivered_can = None;
+std::set<CanType> choices;
+
+void add_choice(CanType canType) {
+  choices.insert(canType);
+}
+
+void deliver(CanType choice) {
+  std::set<CanType>::iterator result = choices.find(choice);
+  if (result != choices.end()) 
+    delivered_can = *result;
+  else 
+    delivered_can = None;
+}
 
 TEST(vending_machine_test, choiceless_machine_deliver_nothing)
 {
-  ASSERT_EQ(1,0);
+  deliver(Cola);
+  ASSERT_EQ(None,delivered_can);
 }
+
+TEST(vending_machine_test, machine_with_cola_delivers_cola_when_asked_for)
+{
+  add_choice(Cola);
+  deliver(Cola);
+  ASSERT_EQ(Cola, delivered_can);
+}
+
+TEST(vending_machine_test, machine_with_fanta_delivers_fanta_when_asked_for)
+{
+  add_choice(Fanta);
+  add_choice(Cola);
+  deliver(Fanta);
+  ASSERT_EQ(Fanta, delivered_can);
+}
+
+
