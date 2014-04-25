1855de0301e3ac93c802a5bb250c8d8a77f3ad6a
Rob Westgeest <rob@qwan.eu>
2014-04-25 15:29:00 +0200

    made start of exercise point

--- include/vending_machine.h
+++ include/vending_machine.h
@@ -1,34 +1,4 @@
 #ifndef _VENDING_MACHINE_H
 #define _VENDING_MACHINE_H
 
-enum CanType { None, Cola, Fanta, Beer };
-
-class Bin {
-  public :
-    virtual void accept(CanType deliveredCan) = 0;
-};
-
-class VendingMachine {
-  public:
-    VendingMachine(Bin& deliverTo) : bin(deliverTo) {}
-
-    void add_choice(CanType canType) {
-      choices.insert(canType);
-    }
-
-    void deliver(CanType choice) {
-      std::set<CanType>::iterator result = choices.find(choice);
-      if (result != choices.end()) 
-        bin.accept(*result);
-      else 
-        bin.accept(None);
-    }
-
-
-  private:
-    std::set<CanType> choices;
-    Bin& bin;
-};
-
-
 #endif // _VENDING_MACHINE_H
--- test/vending_machine_test.cpp
+++ test/vending_machine_test.cpp
@@ -2,39 +2,12 @@
 #include <set>
 #include "vending_machine.h"
 
-class vending_machine_test : public ::testing::Test, public Bin {
 
-  public:
-    void accept(CanType deliveredCan) {
-      delivered_can = deliveredCan;
-    }
 
-  protected:
-    vending_machine_test() : machine(*this), delivered_can(None) {}
-
-    virtual void SetUp() {
-      machine.add_choice(Fanta);
-      machine.add_choice(Cola);
-    }
-
-    CanType deliver(CanType choice) {
-      machine.deliver(choice);
-      return delivered_can;
-    }
-
-    VendingMachine machine;
-    CanType delivered_can;
-};
-
-TEST_F(vending_machine_test, illegal_choice_delivers_nothing)
+TEST(vending_machine_test, choiceless_machine_delivers_nothing)
 {
-  ASSERT_EQ(None,deliver(Beer));
+  ASSERT_EQ(0, 1);
 }
 
-TEST_F(vending_machine_test, machine_delivers_can_of_choice)
-{
-  ASSERT_EQ(Cola, deliver(Cola));
-  ASSERT_EQ(Fanta, deliver(Fanta));
-}
 
 
